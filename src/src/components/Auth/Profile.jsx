309e1ce431e8551b4be2f94eed95a616395ee5ed
Viktor <SuvorovViktorA@gmail.com>
2020-12-02 09:40:42 +0300

    Changed fetch to axios,add prop-types

---
+++ src/src/components/Auth/Profile.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import AllBooks from '../Books/AllBooks';
+import Header from '../Header/Header';
+import FilterBy from '../FilterBy/FilterBy';
+
+const Profile = ({ setAuth }) => {
+  const [name, setName] = useState('');
+
+  const getName = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/profile', {
+        headers: { Authorization: localStorage.token },
+      });
+      const parsRes = await response.data;
+      setName(parsRes.name);
+    } catch (error) {
+      console.error(console.log(error));
+    }
+  };
+
+  useEffect(() => {
+    getName();
+  }, []);
+
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    setAuth(false);
+  };
+  return (
+    <>
+      <div className="container">
+        <Header logout={(e) => logout(e)} name={name} />
+        <Row>
+          <Col sm={3}><FilterBy /></Col>
+          <Col sm={9}><AllBooks /></Col>
+        </Row>
+      </div>
+    </>
+  );
+};
+
+Profile.propTypes = {
+  setAuth: PropTypes.func.isRequired,
+};
+
+export default Profile;
